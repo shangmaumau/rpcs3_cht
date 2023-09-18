595ba66cffe960dbc856f25ef3e209a4c51a57ba
尚毛毛 <shangmaumau@live.com>
2023-09-18 09:08:22 +0800

    修正翻译，避免歧义

--- qt6/translations/rpcs3_zh_CN.ts
+++ qt6/translations/rpcs3_zh_CN.ts
@@ -5434,7 +5434,7 @@ Notes:
 Some games will crash.
 
 Please increase the limit before running RPCS3.</source>
-        <translation>当前最大文件描述器的限制数量过低。
+        <translation>当前的文件描述器的最大限制数量过低。
 有些游戏会崩溃。
 
 请在运行 RPCS3 之前增加限制数量。</translation>
